c7e705a57516665f7e15c322047b4c5692bf16af
Puru Dahal <pdahal@me.com>
2014-09-14 23:16:36 -0700

    ErrorHandelling - check to see if we already have wores.csv created, if not create one

--- anagram.rb
+++ anagram.rb
@@ -2,7 +2,9 @@ class Anagrams
 
   # Collect dictionary words from OSX /usr/share/dict/words 
   def collect_words
-    ` cat /usr/share/dict/words | tee words.csv `
+    ` if [ ! -f dictionary_words.csv ]; then
+    cat /usr/share/dict/words | tee words.csv
+    fi `
   end
 
 end
